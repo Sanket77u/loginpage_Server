c4b3954d34986831fc102d0f79f71efb7c7cbdac
Sanket77u <sanketuphade77@gmail.com>
2025-09-02 15:39:41 +0530

    bug fix

--- Configurations/connectDB.js
+++ Configurations/connectDB.js
@@ -2,26 +2,19 @@ require('dotenv').config();
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    database: process.env.DB_DATABASE,
-    password: process.env.DB_PASS,
-    port: process.env.DB_PORT,
-    ssl: {
-    rejectUnauthorized: false,
-    require: true
-  },
-  statement_timeout: 10000, 
-  idle_in_transaction_session_timeout: 10000
-})
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
 
 pool.connect()
-    .then(client => {
-        console.log("✅ PostgreSQL connected successfully...");
-        client.release();
-    })
-    .catch(err => {
-        console.error("❌ PostgreSQL connection error:", err.message);
-    });
+  .then(client => {
+    console.log("✅ PostgreSQL connected successfully...");
+    client.release();
+  })
+  .catch(err => {
+    console.error("❌ PostgreSQL connection error:", err.message);
+  });
 
 module.exports = pool;
